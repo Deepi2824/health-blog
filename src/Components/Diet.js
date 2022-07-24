75f7011fe7aae3909ab79fe322c14bd48942e8ea
Deepika <vinithdeepika1@gmail.com>
2022-07-24 22:59:32 +0530

    diet.js alignment fixing

--- src/Components/Diet.js
+++ src/Components/Diet.js
@@ -6,12 +6,12 @@ export const Diet = () => {
     <div>
       <div className="bg-[#e5e5e5] w-full lg:[30px] p-10">
         <div className="lg:flex lg:w-[1200px] mx-auto lg:py-32">
-          <div className="lg:w-1/2 lg:px-10">
-            <p className="lg:text-[40px] text-[26px] font-bold leading-tight">
+          <div className="lg:w-1/2 md:px-10">
+            <p className="md:text-[40px] text-[26px] font-bold leading-tight">
               Be mindful – take your time and notice what you eat
             </p>
             <div className="lg:w-[354px] h-[15px] bg-[#6a4aa1] my-7"></div>
-            <p className="lg:text-xl text-lg leading-7 mb-5">
+            <p className="md:text-xl text-lg leading-7 mb-10">
               Dui ut ornare lectus sit amet est placerat. Diam phasellus
               vestibulum lorem sed risus. Amet commodo nulla facilisi nullam
               vehicula ipsum. Amet consectetur adipiscing elit pellentesque. In
@@ -19,7 +19,7 @@ export const Diet = () => {
               proin sed libero enim sed faucibus. Iaculis eu non diam phasellus.
               Elit eget gravida cum sociis natoque. Quis vel eros donec ac odio.
             </p>
-            <button className="bg-[#6a4aa1] hover:bg-[#4d4d4d]  text-white font-bold px-14 py-4 pointer mb-5">
+            <button className="bg-[#6a4aa1] hover:bg-[#4d4d4d]  text-white font-bold px-14 py-4 pointer mb-10">
               READ MORE
             </button>
           </div>
